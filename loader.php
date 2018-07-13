9394b011540fd4b90c800100150eb7561da1cb82
AhmedMedo <ahmed.alaa.eldin.hamdy@gmail.com>
2018-07-13 21:28:36 +0200

    A

--- loader.php
+++ loader.php
@@ -1,7 +1,7 @@
 <?php
 require("vendor\autoload.php");
 
-require(__DIR__.'\Interfaces\DataReader.php');
+include __DIR__.'\Interfaces\DataReader.php' ;
 include __DIR__.'\DataCollector\ApiCollectors.php';
 include __DIR__.'\DataCollector\FileCollectors.php';
 include __DIR__.'\Consumer.php';
